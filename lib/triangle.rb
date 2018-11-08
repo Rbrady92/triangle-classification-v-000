f036651f232db522081306cfa370cf93c0419fe2
Ryan Brady <ryanbrady369@gmail.com>
2018-11-08 23:11:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @arr = [a, b, c].sort
+  end
+
+  def kind
+    if @arr[2] > @arr[0] + @arr[1]
+      begin
+        raise TriangleError
+      rescue TriangleError =>
+        puts error.message
+      end
+    else
+      if @a == @b && @b == @c
+        :equilateral
+      elsif @a == @b || @a == @c || @b == @c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
